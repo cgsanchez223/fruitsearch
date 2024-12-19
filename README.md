192a1e9ed2181dc6f5d69cc3e32116f14cb80349
Christopher Sanchez <cgsanchez223@gmail.com>
2024-12-19 15:07:09 -0500

    update readme

---
+++ README.md
@@ -0,0 +1,17 @@
+Welcome to the Fruit Search Project where you can search for fruit.
+
+____________________________________________________________________
+This project was created for the Software Engineering bootcamp at Stony Brook University.
+
+____________________________________________________________________
+The purpose of this project is to study predictive text.
+- We are given an array containing a list of fruits
+- We had to write a program that listened for key inputs and perdicted written text based on the array of fruit items.
+- We used a search bar to accept a string text that will be matched up with the array string
+- We wrote 2 suggestion functions which handle matching up input with word list, and removing suggestions once they do not meet criteria.
+- Once a keyword is clicked, it will be shown on the search bar and all other suggestions will disappear.
+
+____________________________________________________________________
+Project was created with HTML, CSS, and JavaScript.
+- CSS styling included hover and highlighting functions when clicking on a suggested word.
+- Some fruits had icons, taken from what is available on VS Code
--- app.js
+++ app.js
@@ -3,27 +3,28 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
+// The first search function covers the initial typing in the search box
 function search(str) {
 	let results = [];
-    const word = str.toLowerCase();
+    const word = str.toLowerCase(); // put to lower case to avoid case sensitivity
 
-    for (i = 0; i < fruit.length; i++) {
-        if (fruit[i].toLowerCase().indexOf(word) > -1) {
-            results.push(fruit[i]);
+    for (i = 0; i < fruit.length; i++) { // i starts out at 0. Characters match up with fruit array length. If characters go past length, fruit suggestions dissappear
+        if (fruit[i].toLowerCase().indexOf(word) > -1) { //indexOf searches inside of array
+            results.push(fruit[i]); // Generates fruit results. If results meets above criteria, the selected fruit value is pushed to the results array
         }
     }
 	return results;
 }
-
+// searchHandler looks for the event, typing the words in. Goal is to generate showSuggestions.
 function searchHandler(e) {
     const inputVal = e.currentTarget.value;
     let results = [];
-    if (inputVal.length > 0) {
+    if (inputVal.length > 0) { // match up the inputValue with the search.
         results = search(inputVal);
     }
 	showSuggestions(results);
 }
-
+// showSuggestions generates the fruit results.
 function showSuggestions(results) {
     suggestions.innerHTML = '';
 
@@ -31,14 +32,15 @@ function showSuggestions(results) {
         for (i = 0; i < results.length; i++) {
             suggestions.innerHTML += `<ul>${results[i]}</ul>`;
         }
-        suggestions.classList.add('has-suggestions');
+        suggestions.classList.add('has-suggestions'); // To generate further suggestions as it matches up
     } else {
         results = [];
         suggestions.innerHTML = '';
-        suggestions.classList.remove('has-suggestions');
+        suggestions.classList.remove('has-suggestions'); // To remove suggestuins once they don't match up
     }
 }
 
+// useSuggestion gives the ability to click on suggestion and add it to search bar
 function useSuggestion(e) {
 	input.value = e.target.innerText;
     input.focus();
@@ -47,5 +49,5 @@ function useSuggestion(e) {
 
 }
 
-input.addEventListener('keyup', searchHandler);
-suggestions.addEventListener('click', useSuggestion);
+input.addEventListener('keyup', searchHandler); // Check if key was released
+suggestions.addEventListener('click', useSuggestion); // Looks for the click to add to the search bar
